2e423af8a098ee832b070825545af4fcea5a3b10
Maxime Piraux <maxime.piraux@uclouvain.be>
2022-05-17 13:32:32 +0200

    Consistent labels

--- plots/main_plot.py
+++ plots/main_plot.py
@@ -32,25 +32,25 @@ def main(argv):
     ext = retrieve_extension(args.extension)
 
     if args.metric=="ECMP":
-        #plot_ecmp_imbalance("../csv/ecmp-agg-values-all.csv", "../csv/ecmp-agg-total-all.csv",
-        #                    "../figures/ecmp-imbalance."+ext)
-        #plot_ecmp_imbalance_time_series(["../csv_march_2022/ecmp-diffs-all.yaml"], "ECMP difference", "../figures/ecmp-ts-march-2022."+ext, 0, 20)
-        # plot_all_ecmp_imbalance_in_cdf(["../csv/ecmp-diffs-all.yaml", "../csv/ecmp-diffs-ovh.yaml", "../csv/ecmp-diffs-external.yaml"], ["All", r"\textsc{Ovh}", "External"], "ECMP imbalance (\%)", "../figures/ecmp-diff-cdf."+ext, (0, 10))
-        plot_all_ecmp_imbalance_in_cdf(["../csv/ecmp-nb-links-all.yaml", "../csv/ecmp-nb-links-ovh.yaml", "../csv/ecmp-nb-links-external.yaml"], ["All", r"\textsc{Ovh}", "External"], "Nb ECMP links", "../figures/ecmp-nb-links-cdf."+ext, (0, 20))
+        plot_ecmp_imbalance("../csv/ecmp-agg-values-all.csv", "../csv/ecmp-agg-total-all.csv",
+                            "../figures/ecmp-imbalance."+ext)
+        plot_ecmp_imbalance_time_series(["../csv_march_2022/ecmp-diffs-all.yaml"], "ECMP difference", "../figures/ecmp-ts-march-2022."+ext, 0, 20)
+        #plot_all_ecmp_imbalance_in_cdf(["../csv/ecmp-diffs-all.yaml", "../csv/ecmp-diffs-ovh.yaml", "../csv/ecmp-diffs-external.yaml"], ["All", r"\textsc{OVH}", "Peering"], "ECMP imbalance (\%)", "../figures/ecmp-diff-cdf."+ext, (0, 10))
+        #plot_all_ecmp_imbalance_in_cdf(["../csv/ecmp-nb-links-all.yaml", "../csv/ecmp-nb-links-ovh.yaml", "../csv/ecmp-nb-links-external.yaml"], ["All", r"\textsc{OVH}", "Peering"], "Nb ECMP links", "../figures/ecmp-nb-links-cdf."+ext, (0, 20))
 
     if args.metric == "LOADS":
         # plot_load_time_series(["../csv_march_2022/loads-all.yaml"], "Links load", "../figures/load-ts-march-2022."+ext, 0, 100)
         plot_load_boxplot_week(["../csv/loads-all.yaml"], "Links load (\%)", "../figures/load-ts."+ext, 0, 100)
-        plot_all_loads_in_cdf(["../csv/loads-all.yaml", "../csv/loads-ovh.yaml", "../csv/loads-external.yaml"], ["All", "OVH", "External"], "CDF", "../figures/load-cdf."+ext)
+        plot_all_loads_in_cdf(["../csv/loads-all.yaml", "../csv/loads-ovh.yaml", "../csv/loads-external.yaml"], ["All", r"\textsc{OVH}", "Peering"], "CDF", "../figures/load-cdf."+ext)
         plot_one_boxplot_per_day(["../csv/loads-all.yaml"], "Links load (\%)", "../figures/load-ts-week."+ext, 0, 100)
 
     if args.metric=="Infrastructure":
         plot_infra_evol(["../csv/nb-nodes-ovh.csv"],
-        '\# Routers', [r"\textsc{Ovh}"] , "../figures/nb-nodes-evolution."+ext, 108, 124)
+        '\# Routers', [r"\textsc{OVH}"] , "../figures/nb-nodes-evolution."+ext, 108, 124)
         plot_infra_evol(["../csv/nb-links-all.csv", "../csv/nb-links-ovh.csv", "../csv/nb-links-external.csv"],
         '\# Links', ['All', 'Internal', 'Peering'] , "../figures/nb-links-evolution."+ext, 100, 1000)
         plot_node_degree(["../csv/static_node_degree_internal.csv"],
-        ["\textsc{Ovh}"], "../figures/node-degree_09_05."+ext)
+        [r"\textsc{OVH}"], "../figures/node-degree_09_05."+ext)
 
     if args.metric=="Timeline":
         #plot_timeline_dataset("../figures/timeline."+ext)

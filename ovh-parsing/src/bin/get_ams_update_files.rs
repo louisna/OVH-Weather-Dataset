3a60cad0098b598747a495ec9cfeee5c80e993a9
nrybowski <nicolas.rybowski@uclouvain.be>
2022-09-14 10:03:13 +0200

    Update PeeringDB script with new architecture

--- ovh-parsing/src/bin/get_ams_update_files.rs
+++ ovh-parsing/src/bin/get_ams_update_files.rs
@@ -1,10 +1,8 @@
 use std::sync::mpsc::channel;
-
 use clap::Parser;
-use ovh_parsing::{get_files::get_all_ovh_files, parse_yaml, FileMetadata, Link, OvhData};
-use std::collections::HashMap;
+use ovh_parsing::{OvhData, FileMetadata, parse_yaml, get_files::get_all_ovh_files, Link};
 use threadpool::ThreadPool;
-
+use std::collections::HashMap;
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
@@ -15,13 +13,13 @@ struct Args {
     #[clap(short, long, value_parser, default_value_t = 4)]
     nb_threads: usize,
 }
-
 fn get_results(files: &[FileMetadata], nb_threads: usize) {
     let pool = ThreadPool::new(nb_threads);
     let (tx, rx) = channel();
-
-    let filtered_files = files; // TODO
-
+    let filtered_files = files.iter().filter_map(|f| if
+        f.filepath.split("/").last().unwrap().split("_").last().unwrap().split(".").nth(0).unwrap().parse::<u64>().unwrap() >= 1646365511 &&
+        f.filepath.split("/").last().unwrap().split("_").last().unwrap().split(".").nth(0).unwrap().parse::<u64>().unwrap() <= 1647575112
+        {Some(f)} else {None} ).collect::<Vec<&FileMetadata>>();
     filtered_files.iter().for_each(|file| {
         let tx = tx.clone();
         let s = file.filepath.to_owned();
@@ -35,9 +33,7 @@ fn get_results(files: &[FileMetadata], nb_threads: usize) {
             }
         })
     });
-
     let mut output: Vec<OvhData> = Vec::with_capacity(filtered_files.len());
-
     for _ in 0..files.len() {
         match rx.recv() {
             Err(e) => println!("Error: {:?}", e),
@@ -45,19 +41,33 @@ fn get_results(files: &[FileMetadata], nb_threads: usize) {
             Ok(Some(v)) => output.push(v),
         }
     }
-
     output.sort_by(|a, b| a.timestamp.partial_cmp(&b.timestamp).unwrap());
-
-    // TODO
+    for r in output {
+        println!("{:#?} {:#?}",
+            r.timestamp,
+            r.data
+            .iter()
+            .filter_map(|(_, r)|
+                if r.is_external() && r.name == "AMS-IX" {Some((&r.name, r.peers.clone()))} // downstream
+                else if r.has_external() {
+                    //println!("{:#?}", r);
+                    match r.peers.get("AMS-IX") {
+                        Some(entry) => Some((&r.name, HashMap::from([("AMS-IX".to_string(), entry.clone())]))),
+                        None => None
+                    }
+                }
+                else {None}
+            )
+            .collect::<HashMap<&String, HashMap<String, Vec<Link>>>>()
+            //[&"AMS-IX".to_string()]
+        );
+    }
 }
-
 fn main() {
     let args = Args::parse();
-
     let files = match get_all_ovh_files(&args.dir) {
         Ok(f) => f,
         Err(e) => panic!("Error when getting the files: {}", e),
     };
-
     get_results(&files, args.nb_threads);
 }
